70b0683e5f60cb1724e0b238821825ec4aae08cd
Subramanya K S <subramanyaks2205@gmail.com>
2022-05-06 11:57:38 +0530

    updated structure

--- Structure/structure.go
+++ Structure/structure.go
@@ -15,6 +15,11 @@ func main() {
 	a1 := Address{"Subramanya", "Shimoga", 577201}
 
 	fmt.Println("Address1: ", a1)
-	a2 := Address{Name: "Delhi"}
+	//Acessing member
+	fmt.Println("Name: ", a1.Name)
+
+	a2 := Address{city: "Delhi"}
 	fmt.Println("Address2: ", a2)
+	a2.Name = "Pratheek"
+	fmt.Println("Adress2 (modifier): ", a2)
 }
