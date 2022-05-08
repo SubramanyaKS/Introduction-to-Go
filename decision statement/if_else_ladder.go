f1bec2481d921abde6af284a539a101e980c463f
Subramanya K S <subramanyaks2205@gmail.com>
2022-05-08 19:27:15 +0530

    else if ladder added

---
+++ decision statement/if_else_ladder.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	var v1 int = 1000
+	if v1 == 100 {
+		fmt.Printf("Value of v1 is 100\n")
+
+	} else if v1 == 200 {
+
+		fmt.Printf("Value of a is 200\n")
+
+	} else if v1 == 300 {
+
+		fmt.Printf("Value of a is 300\n")
+
+	} else {
+		fmt.Printf("None of the values is matching\n")
+	}
+}
