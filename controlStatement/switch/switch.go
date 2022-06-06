a4c052f0e83c3657634270be18ac13f49a59b138
Subramanya K S <subramanyaks2205@gmail.com>
2022-06-06 07:34:10 +0530

    added switch

---
+++ controlStatement/switch/switch.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Print("Enter a number: ")
+	var input int64
+	fmt.Scanf("%f", &input)
+	switch i {
+	case 0:
+		fmt.Println("Zero")
+	case 1:
+		fmt.Println("One")
+	case 2:
+		fmt.Println("Two")
+	case 3:
+		fmt.Println("Three")
+	case 4:
+		fmt.Println("Four")
+	case 5:
+		fmt.Println("Five")
+	default:
+		fmt.Println("Unknown Number")
+	}
+
+}
