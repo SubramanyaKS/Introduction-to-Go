7443333106a21837f268b3411fb6aed2830b4d74
Subramanya K S <subramanyaks2205@gmail.com>
2022-06-06 07:25:01 +0530

    added HelloWorld

---
+++ HelloWorld/HelloWorld.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+// Main function
+func main() {
+
+	fmt.Println("!... Hello World ...!")
+	fmt.Println(len("Hello, World"))
+	fmt.Println("Hello, World"[1])
+	fmt.Println("Hello, " + "World")
+
+}
