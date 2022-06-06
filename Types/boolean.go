02f68d624496aa519d9fd12de7b958033b3db51d
Subramanya K S <subramanyaks2205@gmail.com>
2022-06-06 07:25:22 +0530

    added boolean

---
+++ Types/boolean.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("1+1=", 1+1)
+	//boolean operation
+	fmt.Println(true && true)
+	fmt.Println(true && false)
+	fmt.Println(!true)
+	fmt.Println(true || true)
+	fmt.Println(true || false)
+
+}
